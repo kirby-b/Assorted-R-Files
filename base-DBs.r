cbfb5e0abecf07226926f7b11c88648c123db166
kirby-b <kirby.brodie66@gmail.com>
2024-07-04 15:55:14 -0400

    Create base-DBs.r
    I'm gonna use the plant growth one for testing base ones. I would use the crime ones, but it seems extreme for something committed on GitHub

---
+++ base-DBs.r
@@ -0,0 +1,9 @@
+# Im not fully sure what I am doing but this sounded interesting
+
+# LOAD DATASETS PACKAGES ###################################
+
+library(datasets)  # Load/unload base packages manually
+
+# LOAD A DATASET ################################################
+
+head(PlantGrowth)
