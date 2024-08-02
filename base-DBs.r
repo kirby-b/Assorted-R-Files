49188edfe729b48cb046ab000bd107f60c649475
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-08-02 10:46:34 -0400

    More calculations(Max, Min, Mean, Mode, ETC.)
    Added bits to calculate:
    Mean, Median, Mode
    Max and Min
    75 and 50 percent quantiles

--- base-DBs.r
+++ base-DBs.r
@@ -8,3 +8,16 @@ library(datasets)  # Load/unload base packages manually
 
 plants <- PlantGrowth
 
+# Maximum and Minimum values(Highest and Lowest)
+max(plants$weight)
+min(plants$weight)
+
+# Mean, Median, and Mode values(Average, Middle, and Most Common)
+mean(plants$weight)
+median(plants$weight)
+names(sort(-table(plants$weight)))[1]
+
+# Shows the maximum weight that 75 percent of plants are under
+quantile(plants$weight, c(0.75))
+# Shows the maximum weight that 50 percent of plants are under
+quantile(plants$weight, c(0.50))
