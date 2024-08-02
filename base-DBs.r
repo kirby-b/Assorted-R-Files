a0023c5773e1ab7c10e29c998e194ccbd6830e8f
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-08-02 13:52:02 -0400

    Loops and averages for a bar chart
    Its probably messy and imperfect, but it was fun and felt like a good start/ example for using R

--- base-DBs.r
+++ base-DBs.r
@@ -23,13 +23,36 @@ quantile(plants$weight, c(0.75))
 quantile(plants$weight, c(0.50))
 
 # Holds the weight averages for the three groups
-weightavg <- c()
+weightavg <- c(0,0,0)
 # Holds the three group names
 group <-c("Ctrl", "Trt1", "Trt2")
 
+# Holds values for iteration, counts of how many entries, and total weight
+i <- 1
+con <- concount <- 0
+t1 <- t1count <- 0
+t2 <- t2count <- 0
+# Loops through the plants table and calculate averages
 for (x in plants$weight){
-  #Will loop through the plants table and calculate averages 
+  # Checks the group each value belongs to and adds it to that groups total
+  if (plants$group[i] == "ctrl"){
+    con <- con + 10
+    concount <- concount + 1
+  }
+  else if (plants$group[i] == "trt1"){
+    t1 <- t1 + x
+    t1count <- t1count + 1
+  }
+  else if (plants$group[i] == "trt2"){
+    t2 <- t2 + x
+    t2count <- t2count + 1
+  }
+  i <- i + 1
 }
+# Calculates the averages and adds them to the weight average vector
+weightavg[1] <- (con/concount)
+weightavg[2] <- (t1/t1count)
+weightavg[3] <- (t2/t2count)
 
 # Makes a bar chart for the average plant weight in the three groups
-barplot(weightavg, group)
+barplot(weightavg, names.arg = group, main="Average Plant Weight by Group")
